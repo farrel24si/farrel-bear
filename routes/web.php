a8867ceecf5ddc76066bf10b6ce4a47414c53f27
farrel24si <farrel24si@mahasiswa.pcr.ac.id>
2025-10-14 11:26:33 +0700

    Membuat Sistem Data Pelanggan

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\PegawaiController;
 use App\Http\Controllers\QuestionController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\MahasiswaController;
+use App\Http\Controllers\PelangganController;
 use App\Http\Controllers\MatakuliahController;
 
 Route::get('/', function () {
@@ -52,3 +53,7 @@ Route::post('question/store', [QuestionController::class, 'store'])
 		->name('question.store');
 
 route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+
+Route::resource('pelanggan', PelangganController::class);
+
+

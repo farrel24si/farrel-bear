94f1f870c30fcb3fa8ba6989ed96067fa3fc0094
farrel24si <farrel24si@mahasiswa.pcr.ac.id>
2025-10-21 11:27:49 +0700

    Membuat CRUD user

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\UserController;
 use App\Http\Controllers\PegawaiController;
 use App\Http\Controllers\QuestionController;
 use App\Http\Controllers\DashboardController;
@@ -56,4 +57,5 @@ route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard
 
 Route::resource('pelanggan', PelangganController::class);
 
+Route::resource('user', UserController::class);
 

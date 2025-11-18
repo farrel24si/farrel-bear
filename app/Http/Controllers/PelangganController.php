943cf8a8fae4d0c3f68abd5f24b3786f7a93d21a
farrel24si <farrel24si@mahasiswa.pcr.ac.id>
2025-11-18 08:20:34 +0700

    Membuat filter

--- app/Http/Controllers/PelangganController.php
+++ app/Http/Controllers/PelangganController.php
@@ -9,9 +9,16 @@ class PelangganController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
-        $data['dataPelanggan'] = Pelanggan::paginate(10);
+        $filterableColumns = ['gender'];
+        $searchableColumns = ['first_name', 'last_name', 'email'];
+
+        $data['dataPelanggan'] = Pelanggan::filter($request, $filterableColumns)
+        ->search($request, $searchableColumns)
+        ->paginate(10)
+        ->withquerystring();
+
         return view('admin.pelanggan.index', $data);
     }
 
